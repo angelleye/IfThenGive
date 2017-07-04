417076febecc91f4271bb9e2223f06f37264ed32
tejas <tejasmht88@gmail.com>
2017-07-04 12:55:27 +0530

    Condition to check for sandbox is enabled or not., ref #11

--- admin/partials/class-give-when-html-format.php
+++ admin/partials/class-give-when-html-format.php
@@ -353,7 +353,7 @@ class AngellEYE_Give_When_interface {
         $PayPal_config = new Give_When_PayPal_Helper();   
         $paypal_account_id = get_option('give_when_permission_connected_person_payerID');
         $PayPal_config->set_api_subject($paypal_account_id);
-        $PayPal = new Angelleye_PayPal($PayPal_config->get_configuration());
+        $PayPal = new Angelleye_PayPal($PayPal_config->get_configuration());        
         $GTDFields = array(
             'transactionid' => $transaction_id
         );
